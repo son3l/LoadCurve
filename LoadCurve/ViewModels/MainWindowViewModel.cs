f79fb416b5dfd555f5b426eb96b2f3f7a42383a0
aizen <zell_03@mail.ru>
2024-11-10 13:42:16 +0700

    add todo

--- LoadCurve/ViewModels/MainWindowViewModel.cs
+++ LoadCurve/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,7 @@ namespace LoadCurve.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    //TODO 
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
